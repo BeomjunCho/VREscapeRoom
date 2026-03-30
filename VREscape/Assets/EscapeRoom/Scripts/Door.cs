cb815f23e11483cf71ef6928bb60ff863255559d
matty-li <madelineli.van@gmail.com>
2026-03-30 03:43:54 -0700

    Interactable doors

---
+++ VREscape/Assets/EscapeRoom/Scripts/Door.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float openAngle = 90f;
+
+    private bool isOpen = false;
+    private float targetY = -90f;
+    private Transform player;
+
+    private void Start()
+    {
+        transform.localRotation = Quaternion.Euler(0f, -90f, 0f);
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        float currentY = transform.localEulerAngles.y;
+        transform.localRotation = Quaternion.Euler(
+            0f,
+            Mathf.LerpAngle(currentY, targetY, Time.deltaTime * speed),
+            0f
+        );
+    }
+
+    public void ToggleDoor()
+    {
+        if (isOpen)
+        {
+            targetY = -90f;
+        }
+        else
+        {
+            Vector3 toPlayer = player.position - transform.position;
+            float dot = Vector3.Dot(transform.right, toPlayer);
+            targetY = dot >= 0f ? -90f + openAngle : -90f - openAngle;
+        }
+
+        isOpen = !isOpen;
+    }
+}
