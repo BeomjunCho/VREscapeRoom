2a17b38f20cad671b3b533c1ab72a0b00ecb1b1a
matty-li <madelineli.van@gmail.com>
2026-03-31 22:34:14 -0700

    Awesome menu
    Lets you turn off vignette

--- VREscape/Assets/EscapeRoom/Scripts/UI/LocomotionToggle.cs
+++ VREscape/Assets/EscapeRoom/Scripts/UI/LocomotionToggle.cs
@@ -3,22 +3,45 @@ using UnityEngine.UI;
 
 public class LocomotionToggle : MonoBehaviour
 {
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private Color normalColor;
+
     [SerializeField] private Toggle teleport;
     [SerializeField] private GameObject[] teleportObjects = new GameObject[0];
 
     [SerializeField] private Toggle slide;
     [SerializeField] private GameObject[] slideObjects = new GameObject[0];
 
+    [SerializeField] private Toggle vignette;
+    [SerializeField] private GameObject vignetteObject;
     private void Awake()
     {
-        slide.onValueChanged.AddListener(isOn => { if (isOn) SetLocomotionMode(true); });
-        teleport.onValueChanged.AddListener(isOn => { if (isOn) SetLocomotionMode(false); });
+        slide.onValueChanged.AddListener(_ => SyncAll());
+        teleport.onValueChanged.AddListener(_ => SyncAll());
+        vignette.onValueChanged.AddListener(_ => SyncAll());
+        SyncAll();
+    }
 
+    private void SyncAll()
+    {
         SetLocomotionMode(slide.isOn);
+        vignetteObject.SetActive(vignette.isOn);
     }
     private void SetLocomotionMode(bool locomotion)
     {
         foreach (GameObject obj in slideObjects) { obj.SetActive(locomotion); }
         foreach (GameObject obj in teleportObjects) { obj.SetActive(!locomotion); }
+
+        teleport.targetGraphic.color = teleport.isOn
+            ? selectedColor
+            : normalColor;
+
+        slide.targetGraphic.color = slide.isOn
+            ? selectedColor
+            : normalColor;
+
+        vignette.targetGraphic.color = slide.isOn
+           ? selectedColor
+            : normalColor;
     }
 }
